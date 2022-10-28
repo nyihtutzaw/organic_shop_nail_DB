71eaebf557a82614f74da30dc72272b5237e963a
kaung-san-hein <kaungsanhein2019@gmail.com>
2022-10-28 16:55:27 +0630

    [Done]change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,17 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const host = "http://organicapi.92134691-30-20190705152935.webstarterz.com/api/v1";
+// export const host = "http://organicapi.92134691-30-20190705152935.webstarterz.com/api/v1";
+export const host = 'https://organic-api.rcs-mm.com/api/v1'
 
 export const setAccessToken = (token) => {
   if (token) {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
   } else {
-    delete axios.defaults.headers.common["Authorization"];
+    delete axios.defaults.headers.common['Authorization']
   }
-};
+}
 
 export const call = async (method, path, data) => {
-  const response = await axios[method](`${host}/${path}`, data);
-  return response.data;
-};
+  const response = await axios[method](`${host}/${path}`, data)
+  return response.data
+}
